1abf8cc921f5bca44252796d1a8aa06085f9aa34
Alex Lynch <mlynch5187@gmail.com>
2020-03-02 12:13:26 -0700

    Create test_can_encrypt_with_key_and_date. Does exactly what it sounds like

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -17,4 +17,9 @@ class EngimaTest < MiniTest::Test
     assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
   end
 
+  def test_can_encrypt_message_with_key_and_date_and_special_char
+    expected = {encryption: "keder, ohulw", key: "02715", date: "040895"}
+    assert_equal expected, @enigma.encrypt("hello, world", "02715", "040895")
+  end
+
 end
