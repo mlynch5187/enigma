f54c7757563c88abec77c8d044e75fc6f244dc64
Alex Lynch <mlynch5187@gmail.com>
2020-03-02 13:28:16 -0700

    Create split_keys method. Uses an @split_keys instance variable which is an empty array in initialize. The keys are shoveled into this array and then using map iterated over to turn them into integers

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,13 +1,15 @@
 class Offset
-  attr_reader :key, :date
+  attr_reader :key, :date, :split_keys
 
   def initialize(key = 5.times.map {rand(0..9)}.join, date = Time.now.strftime("%d%m%y"))
     @key = key
     @date = date
+    @split_keys = []
   end
 
   def split_keys
-    
+    key.split(//).each_cons(2) {|number| @split_keys << number.join}
+    @split_keys.map {|key| key.to_i}
   end
 
 end
