69146c28ea6e05fbb3b32d65b8f8d484cf11ea55
Alex Lynch <mlynch5187@gmail.com>
2020-03-02 15:12:25 -0700

    Created encryption runner file. This file is run when encrypting a message. The encrypted message is output into the encrypted_message.txt file

--- encryption_runner.rb
+++ encryption_runner.rb
@@ -1,4 +1,14 @@
 require './lib/enigma'
 require './lib/offset'
 
-enigma = Enigma.new 
+enigma = Enigma.new
+
+message = File.open(ARGV[0], "r")
+
+incoming_text = message.read
+
+encrypted_message = enigma.encrypt(incoming_text.chomp)
+
+file = File.write(ARGV[1], encrypted_message[:encryption])
+
+puts "Created 'encrypted.txt' with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
