f4d5017c12b96b78d4d4c985623cd4dd47ab3f85
Alex Lynch <mlynch5187@gmail.com>
2020-03-02 12:10:46 -0700

    Create test_can_encrypt_with_key_and_Date test. Method needs to be created in enigma class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,4 +12,9 @@ class EngimaTest < MiniTest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_can_encrypt_message_with_key_and_date
+    expected = {encryption: "keder ohulw", key: "02715", date: "040895"}
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
 end
